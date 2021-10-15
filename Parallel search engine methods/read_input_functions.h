6e4d8ee716942b40a9df7a53e137a9b573d957fe
Andrey-Abrosimov <80051157+Andrey-Abrosimov@users.noreply.github.com>
2021-10-15 19:21:39 +0300

    Create read_input_functions.h

---
+++ Parallel search engine methods/read_input_functions.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <set>
+#include <stdexcept>
+#include <string>
+#include <utility>
+#include <vector>
+#include <queue>
+
+std::string ReadLine() {
+    std::string s;
+    getline(std::cin, s);
+    return s;
+}
+
+int ReadLineWithNumber() {
+    int result;
+    std::cin >> result;
+    ReadLine();
+    return result;
+}
