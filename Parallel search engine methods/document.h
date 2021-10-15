db63a10bbfb6a7db994cb8f89f21367f0aa74671
Andrey-Abrosimov <80051157+Andrey-Abrosimov@users.noreply.github.com>
2021-10-15 19:20:05 +0300

    Create document.h

---
+++ Parallel search engine methods/document.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <set>
+#include <stdexcept>
+#include <string>
+#include <utility>
+#include <vector>
+#include <queue>
+#include <functional>
+#include <execution>
+struct Document {
+    Document() = default;
+
+    Document(int id, double relevance, int rating)
+        : id(id)
+        , relevance(relevance)
+        , rating(rating) {
+    }
+
+    int id = 0;
+    double relevance = 0.0;
+    int rating = 0;
+};
+
+std::ostream& operator<<(std::ostream& out, const Document& document) {
+    out << "{ "
+        << "document_id = " << document.id << ", "
+        << "relevance = " << document.relevance << ", "
+        << "rating = " << document.rating << " }";
+    return out;
+}
