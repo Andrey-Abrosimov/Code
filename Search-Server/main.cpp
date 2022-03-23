46881c19aec5f6934f04445cfa8eb5c9e22c1a90
Andrey-Abrosimov <80051157+Andrey-Abrosimov@users.noreply.github.com>
2022-03-23 19:39:40 +0300

    Create main.cpp

---
+++ Search-Server/main.cpp
@@ -0,0 +1,15 @@
+#include "transport_catalogue.h"
+#include "json.h"
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+
+int main() {
+    ParserAndInfo pai;
+    pai.InputBase(std::cin);
+    
+    return 0;
+}
