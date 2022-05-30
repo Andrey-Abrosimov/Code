5803898ab98fc16d7f3084fc6f7155ef46d439ff
Andrey-Abrosimov <80051157+Andrey-Abrosimov@users.noreply.github.com>
2022-05-30 18:05:44 +0300

    Create main.cpp

---
+++ B/main.cpp
@@ -0,0 +1,14 @@
+#include "transport_catalogue.h"
+#include "json.h"
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    FromMain fm;
+    fm.ParseRequests(std::cin);
+    
+    return 0;
+}
